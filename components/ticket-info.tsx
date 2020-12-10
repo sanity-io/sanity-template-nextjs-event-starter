8f8094a4b64fb59fab8d8cce9b25275d81fc0c4c
Shu Uesugi <shu@chibicode.com>
2020-12-09 23:05:39 -0800

    Remove CONF_URL (#13)

--- components/ticket-info.tsx
+++ components/ticket-info.tsx
@@ -1,8 +1,11 @@
 import styles from './ticket-info.module.css';
 import styleUtils from './utils.module.css';
 import Logo from './logo';
-import { DATE, CONF_URL } from '@lib/constants';
+import { DATE, SITE_URL } from '@lib/constants';
 import VercelLogo from '@components/icons/icon-platform';
+
+const siteUrl = new URL(SITE_URL);
+const siteUrlForTicket = `${siteUrl.host}${siteUrl.pathname}`.replace(/\/$/, '');
 
 export default function TicketInfo({ logoTextSecondaryColor = 'var(--accents-5)' }) {
   const createdBy = (
@@ -21,7 +24,7 @@ export default function TicketInfo({ logoTextSecondaryColor = 'var(--accents-5)'
         <div>ONLINE</div>
       </div>
       <div className={styleUtils['hide-on-mobile']}>{createdBy}</div>
-      <div className={styles.url}>{CONF_URL}</div>
+      <div className={styles.url}>{siteUrlForTicket}</div>
       <div className={styleUtils['show-on-mobile']}>{createdBy}</div>
     </div>
   );
