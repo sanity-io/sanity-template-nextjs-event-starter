f27b7f09d555118f101be6eb704442f2b9cc159e
Shu Uesugi <shu@chibicode.com>
2020-12-12 22:52:16 -0800

    Move view source icon from _app.tsx to layout component (#30)
    * Move view source icon to layout file
    
    * Remove view source from _app.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,6 +7,7 @@ import styles from './layout.module.css';
 import Logo from './icons/icon-logo';
 import MobileMenu from './mobile-menu';
 import Footer, { HostedByVercel } from './footer';
+import ViewSource from '@components/view-source';
 
 type Props = {
   children: React.ReactNode;
@@ -20,43 +21,46 @@ export default function Layout({ children, className, hideNav, layoutStyles }: P
   const activeRoute = router.asPath;
 
   return (
-    <div className={styles.background}>
-      {!hideNav && (
-        <header className={cn(styles.header)}>
-          <div className={styles['header-logos']}>
-            <MobileMenu key={router.asPath} />
-            <Link href="/">
-              {/* eslint-disable-next-line */}
-              <a className={styles.logo}>
-                <Logo />
-              </a>
-            </Link>
-          </div>
-          <div className={styles.tabs}>
-            {NAVIGATION.map(({ name, route }) => (
-              <Link key={name} href={route}>
-                <a
-                  className={cn(styles.tab, {
-                    [styles['tab-active']]: activeRoute.startsWith(route)
-                  })}
-                >
-                  {name}
+    <>
+      <ViewSource />
+      <div className={styles.background}>
+        {!hideNav && (
+          <header className={cn(styles.header)}>
+            <div className={styles['header-logos']}>
+              <MobileMenu key={router.asPath} />
+              <Link href="/">
+                {/* eslint-disable-next-line */}
+                <a className={styles.logo}>
+                  <Logo />
                 </a>
               </Link>
-            ))}
-          </div>
-          <div className={cn(styles['header-right'])}>
-            <HostedByVercel />
-          </div>
-        </header>
-      )}
-      <div className={styles.page}>
-        <main className={styles.main} style={layoutStyles}>
-          <SkipNavContent />
-          <div className={cn(styles.full, className)}>{children}</div>
-        </main>
-        {!activeRoute.startsWith('/stage') && <Footer />}
+            </div>
+            <div className={styles.tabs}>
+              {NAVIGATION.map(({ name, route }) => (
+                <Link key={name} href={route}>
+                  <a
+                    className={cn(styles.tab, {
+                      [styles['tab-active']]: activeRoute.startsWith(route)
+                    })}
+                  >
+                    {name}
+                  </a>
+                </Link>
+              ))}
+            </div>
+            <div className={cn(styles['header-right'])}>
+              <HostedByVercel />
+            </div>
+          </header>
+        )}
+        <div className={styles.page}>
+          <main className={styles.main} style={layoutStyles}>
+            <SkipNavContent />
+            <div className={cn(styles.full, className)}>{children}</div>
+          </main>
+          {!activeRoute.startsWith('/stage') && <Footer />}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,11 @@ import '@styles/nprogress.css';
 import type { AppProps } from 'next/app';
 import NProgress from '@components/nprogress';
 import ResizeHandler from '@components/resize-handler';
-import ViewSource from '@components/view-source';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
       <OverlayProvider>
-        <ViewSource />
         <Component {...pageProps} />
         <ResizeHandler />
         <NProgress />
