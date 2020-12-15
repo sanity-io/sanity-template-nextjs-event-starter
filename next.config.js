de4bc950d857d1987f2082cc98efcbf950ffa7de
Shu Uesugi <shu@chibicode.com>
2020-12-14 22:38:06 -0800

    Remove pageExtensions (#43)

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@
  */
 
 module.exports = {
-  pageExtensions: ['jsx', 'js', 'ts', 'tsx'],
   images: {
     domains: ['datocms-assets.com', 'www.datocms-assets.com'],
     imageSizes: [24, 64, 300]
