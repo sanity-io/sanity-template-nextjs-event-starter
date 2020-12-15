bd2587ff0ffae6e319bab39b4039db9e8dd42334
Lee Robinson <lrobinson2011@gmail.com>
2020-12-15 11:42:35 -0600

    Update meta description for Twitter share.

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,7 +5,7 @@ export const BRAND_NAME = 'ACME';
 export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
 export const SITE_NAME = 'ACME Conf';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created by Vercel.';
+  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
 export const DATE = 'October 27, 2020';
